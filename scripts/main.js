e97d7a1dea31830d202a2ff7172e4caf9d4c4dae
LinkinWD <yaramodesu@gmail.com>
2021-08-16 12:19:06 +0300

    toimiva slider

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,11 @@
 const hamppari = document.querySelector('#hamppari');
 const navlinks = document.querySelector('.navlinks');
 const slider = document.querySelector('.slider')
+const seuraava = document.querySelector('#seuraava')
+const edellinen = document.querySelector("#edellinen")
+const sliderinKuva = document.querySelector('#sliderinkuva')
+
+let dia = 0
 
 const slides = [
     'img/slide1.jpg',
@@ -10,9 +15,34 @@ const slides = [
     'img/slide4.jpg'
 ]
 
+sliderinKuva.src = slides[dia]
+
 hamppari.addEventListener('click', () => {
     hamppari.classList.toggle('avaa')
     navlinks.classList.toggle('aukea')
+})
+
+seuraava.addEventListener('click', ()=> {
+    
+     if(dia < slides.length -1)  {
+        dia++
+        sliderinKuva.src = slides[dia]
+        
+    } else {
+        dia = 0
+        sliderinKuva.src = slides[dia]
+    }  
+})
+edellinen.addEventListener('click', ()=> {
+    
+    if(dia > 0)  {
+       dia--
+       sliderinKuva.src = slides[dia]
+       
+   } else {
+       dia = slides.length -1
+       sliderinKuva.src = slides[dia]
+   }  
 })
 
 anime ({
