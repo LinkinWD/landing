bae0b5462e7691ef7ddca70a858d57674d6b484e
LinkinWD <yaramodesu@gmail.com>
2021-08-24 10:05:48 +0300

    alkusivu alkaa olla ok, nyt värit

--- scripts/animate.js
+++ scripts/animate.js
@@ -2,14 +2,29 @@ AOS.init({
     duration: 1200,
   })
 
+
+const textWrapper = document.querySelector('.animoi');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.animoi .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  });
+
+
+
 anime({
     targets: '.nuoli',
     keyframes: [
         {translateY: 50},
         {translateY:0}
     ],
-    delay: 1700,
-    duration: 1500,
+    delay: 2500,
+    duration: 1000,
     easing: 'spring(1, 80, 10, 0)',
     loop: true
   });
