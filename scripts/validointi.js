a295ee14eaf0201540696f493572f970a1408173
LinkinWD <yaramodesu@gmail.com>
2021-08-25 11:54:22 +0300

    validointi formissa menossa, viesti puuttuu

---
+++ scripts/validointi.js
@@ -0,0 +1,54 @@
+const submit = document.getElementById('submit');
+const wholeName = document.getElementById('name');
+const email = document.getElementById('email');
+
+
+wholeName.addEventListener('keyup', () => {
+    const nimiLabel = document.querySelector('#nimilabel')
+    if(wholeName.value != '') {
+        nimiLabel.textContent = ''
+    } else if (wholeName.value == "") {
+        nimiLabel.textContent = "Etu ja sukunimi"
+    }
+})
+
+email.addEventListener('keyup', () => {
+    const emailLabel = document.querySelector('#emaillabel')
+    if(email.value != '') {
+        emailLabel.textContent = ''
+    } else if (email.value == "") {
+        emailLabel.textContent = "Sähköposti"
+    }
+})
+
+
+function validateNimi(){
+    const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    const nimiVaroitus = document.querySelector('#nimivaroitus')
+    if(!regName.test(wholeName.value)){
+        nimiVaroitus.textContent =('Laittaisitko kokonimesi? (etu. ja sukunimi).');
+        document.getElementById('name').focus();
+        return false;
+    } else {
+        nimiVaroitus.textContent = '';
+        return true
+    }
+}
+
+function ValidateEmail() {
+    const emailVaroitus = document.querySelector('#emailvaroitus') 
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+    {
+        
+        emailVaroitus.textContent = '';
+        return (true)
+    }
+        emailVaroitus.textContent = ("Laita oikean sähköposti osoite!")
+        return (false)
+}
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    validateNimi()
+    ValidateEmail()
+})
